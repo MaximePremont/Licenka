4f47893e1bb751e5744f5c489312596bad342cbd
Maxime PREMONT <maxime.premont@gmail.com>
2022-11-22 13:38:29 +0100

    feat(server) : Added basic express server

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 // Licenka
 // by Pocker Team
 
+const express = require('express')
+const dotenv = require('dotenv')
+
+dotenv.config()
+const app = express()
+const port = process.env.PORT || 80
+
+app.use(express.static('public'))
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST')
+    res.setHeader('Access-Control-Allow-Headers', '*')
+    next()
+})
+
+app.get('/', async(req, res) => {
+    res.redirect("https://github.com/MaximePremont/Licenka")
+})
+
+app.listen(port, () => {
+    console.log(`Licenka server running on port ${port}`)
+})
+
+module.exports = app
