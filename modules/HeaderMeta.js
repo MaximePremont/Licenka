e282494ece9248aab24c91f7820f0b09dc01cca6
Nolann <62215577+Nolann71@users.noreply.github.com>
2022-11-23 11:00:23 +0100

    add(front): add css and tailwind props > > Co-authored-by: MaximePremont <maxime.premont@gmail.com>

--- modules/HeaderMeta.js
+++ modules/HeaderMeta.js
@@ -7,6 +7,7 @@ const HeaderMeta = () => {
             <meta charSet="UTF-8" />
             <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1" />
             <meta name="format-detection" content="telephone=yes" />
+            <link href="https://fonts.cdnfonts.com/css/cabinet-grotesk" rel="stylesheet"/>
         </Head>
     );
 };
--- pages/approve.js
+++ pages/approve.js
@@ -7,7 +7,7 @@ const ApprovePage = () => {
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
         Button
       </button>
     </div>
