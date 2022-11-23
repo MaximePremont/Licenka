8b7e1148099690abe8d5ab6984f2bfe147be8980
Nolann <62215577+Nolann71@users.noreply.github.com>
2022-11-23 13:08:28 +0100

    feat(front): buy page in progress

--- pages/approve.js
+++ pages/approve.js
@@ -1,8 +1,15 @@
+import Image from 'next/image';
 import DefaultLayout from '../modules/layout'
 
 const ApprovePage = () => {
   return (
     <div>
+      <div className="grid grid-rows-3 grid-flow-col gap-4">
+        <div className="row-span-3">01</div>
+        <div className="row-span-3">
+          <Image className="columns-3 ml-3 mt-3" src="/joker.svg" width={652} height={877} alt="logo" />
+        </div>
+      </div>
     </div>
   )
 }
