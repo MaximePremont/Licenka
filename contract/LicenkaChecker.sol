3d40f82041071e48c6762052bb47313c8d29cc03
Slooth <victor.guyot@epitech.eu>
2022-11-24 09:51:09 +0100

    feat(contract): updated to new contrects

--- contract/LicenkaChecker.sol
+++ contract/LicenkaChecker.sol
@@ -6,15 +6,12 @@ import "./ILicenka.sol";
 contract LicenkaChecker {
 
     ILicenka _licenka;
-    // address _licenkaAddress;
 
     constructor(address licenkaAddress) {
         _licenka = ILicenka(licenkaAddress);
-        // _licenkaAddress = licenkaAddress;
     }
 
     modifier mustBeSubscribe(address owner, uint licenseId) {
-        // (bool success, bytes memory data) = _licenkaAddress.delegatecall(abi.encodeWithSignature("verifySubscription(address, uint)", owner, licenseId));
         require(_licenka.verifySubscription(owner, licenseId), "Didn't subscribe");
         _;
     }
