c20eb7d39de0b66ae3cc6d82c536f7ae26dd18b9
Maxime PREMONT <maxime.premont@gmail.com>
2022-11-24 16:39:51 +0100

    feat(deploy) : Added vercel analytics component

--- modules/layout/index.js
+++ modules/layout/index.js
@@ -1,9 +1,11 @@
 import Footer from "../Footer";
 import Header from "../Header";
+import { Analytics } from '@vercel/analytics/react';
 
 const DefaultLayout = ({ children }) => {
     return (
         <div className="relative w-full bg-background">
+            <Analytics />
             <Header/>
             <main>{children}</main>
             <Footer/>
