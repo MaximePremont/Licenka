afcebe4a6d0cf9a1791cfc1094713ff91cf864a5
Mikatech <mikael.vallenet@epitech.eu>
2022-11-27 00:37:01 +0100

    feat(demo): adapt website to licenka 2.0

--- demo/web/src/pages/login.jsx
+++ demo/web/src/pages/login.jsx
@@ -1,6 +1,4 @@
 import logo from "../assets/game-controller.png";
-import user from "../assets/user.png";
-import lock from "../assets/lock.png";
 import powered from "../assets/powered_licenka.png";
 import {useSearchParams} from "react-router-dom";
 
@@ -12,14 +10,10 @@ function Login() {
             <header className="App-header">
                 <h1 className="title">CloudGames</h1>
                 <img src={logo} className="App-logo" alt="logo" />
-                <form method="POST" action="http://localhost:8080/verify">
-                    <input type="text" id="address" className="userInput" name="address" placeholder="ADDRESS"/><br/>
-                    <img src={user} id="logo-user" alt="logo" />
-                    <input type="password" id="password" className="userInput" name="password" placeholder="PASSWORD"/><br/>
-                    <img src={lock} id="logo-lock" alt="logo" />
-                    <input type="submit" id="login" className="button" value="LOGIN"/>
+                <form method="GET" action="https://licenka.space/oauth?redirect=http://localhost:3000/auth&licenseId=5&">
+                    <input type="submit" id="login" className="button" value="Login with Licenka"/>
                 </form>
-                <p id="buying-license"><a href="https://licenka.space/oauth?redirect=http://localhost:3000/auth&licenseId=5&">DON'T HAVE A LICENSE?</a></p>
+                <p id="buying-license"><a href="https://licenka.space/approve?id=5&redirect=http://localhost:3000">DON'T HAVE A LICENSE?</a></p>
                 <p id="warning">{queryParameters.get("warning")}</p>
                 <img src={powered} className="licenka-power" alt="logo"/>
             </header>
