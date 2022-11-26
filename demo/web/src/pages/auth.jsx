272e91fd33625a9ce6d3e81e30fe3296a10ed21c
Mikatech <mikael.vallenet@epitech.eu>
2022-11-27 00:06:03 +0100

    feat(demo): save cookies

--- demo/web/src/pages/auth.jsx
+++ demo/web/src/pages/auth.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import {useSearchParams} from "react-router-dom";
+import Cookies from 'universal-cookie';
 
 function Auth() {
-    return (
+    const [queryParameters] = useSearchParams()
+    if (queryParameters.get("nonce_signed") == null ||
+        queryParameters.get("address") == null) {
+        const cookies = new Cookies();
+        cookies.set("nonce_signed", queryParameters.get("nonce_signed"))
+        cookies.set("address", queryParameters.get("address"))
+    }
+        return (
         <h1 className="title">License verification ...</h1>
     );
 }
