e54c303a954a8d1805a29d2084598b95c5dd4613
Mikatech <mikael.vallenet@epitech.eu>
2022-11-26 22:21:06 +0100

    feat: add signature verification

--- pages/api/wallet/nonce.js
+++ pages/api/wallet/nonce.js
@@ -1,5 +1,5 @@
 const dotenv = require('dotenv')
-dotenv.config()
+dotenv.config();
 const crypto = require('crypto');
 const { connectToDatabase } = require('../../../lib/mongodb');
 
@@ -8,20 +8,20 @@ async function nonce(req, res) {
     let nonce = crypto.randomBytes(16).toString('base64');
 
     const users = db.collection("users");
-    const result = await users.insertOne({ walletAddress: req.query.walletAddress, nonce: nonce})
-    console.log(result)
-    res.status(200).json({ walletAddress: req.query.walletAddress, nonce: nonce})
+    const result = await users.insertOne({ walletAddress: req.query.walletAddress, nonce: nonce});
+    console.log(result);
+    res.status(200).json({ walletAddress: req.query.walletAddress, nonce: nonce});
 }
 
 export default function handler(req, res) {
-    console.log(req.method)
+    console.log(req.method);
     if (req.method === 'GET') {
         if (!req.query.walletAddress) {
-            res.status(400).json({ error: 'Missing walletAddress' })
-            return
+            res.status(400).json({ error: 'Missing walletAddress' });
+            return;
         }
-        nonce(req, res)
-        return
+        nonce(req, res);
+        return;
     }
     res.status(404).json({ message: 'Unknown route' })
 }
--- pages/api/wallet/verify.js
+++ pages/api/wallet/verify.js
@@ -1,48 +1,33 @@
-const dotenv = require('dotenv')
-dotenv.config()
+const dotenv = require('dotenv');
+dotenv.config();
+import { ethers } from "ethers";
+const { connectToDatabase } = require('../../../lib/mongodb');
 
-const axios = require("axios")
-const Web3js = require("web3");
-
-const axiosInstance = axios.create({
-    baseURL: "https://api.starton.io",
-    headers: {
-        "x-api-key": process.env.API_KEY,
-    },
-})
 
 async function verify(req, res) {
-    axiosInstance.post(
-        "/v3/smart-contract/binance-testnet/0x1aE04F30E59f1c38E72E12bd2bD94e7434E218f8/read",
-        {
-            functionName: "verifySubscriptionWeb2",
-            params: [
-                req.query.userAddress,
-                Web3js.utils.keccak256(req.query.userPassword),
-                req.query.licenseId
-            ]
-        }
-    ).then((response) => {
-        console.log(response.data.response)
-        res.status(200).json({ license: response.data.response })
-    }).catch(() => {
-        res.status(400).json({ error: "password not set / wrong password" })
-    })
+    let { db } = await connectToDatabase();
+    const users = db.collection("users");
+    const nonce = await users.findOne({walletAddress: req.query.walletAddress});
+    if (nonce == null) {
+        res.status(500).json({error: 'walletAddress not register in database'})
+    }
+    const signerAddr = ethers.utils.verifyMessage("qsddqdqsd", "0x9c1e97ee80495239effd1a053a018994ced938f40f1584e103660b056a46d5915d4ce2cb7ca67f4e018cf1544602f7683200108ee6d743c10016e53ac8ed5eae1c");
+    if (signerAddr !== req.query.walletAddress) {
+        res.status(200).json({ verified: false });
+        return
+    }
+    res.status(200).json({ verified: true });
 }
 
 export default function handler(req, res) {
     console.log(req.method)
     if (req.method === 'GET') {
-        if (!req.query.userAddress) {
-            res.status(400).json({ error: 'Missing userAddress' })
+        if (!req.query.walletAddress) {
+            res.status(400).json({ error: 'Missing walletAddress' })
             return
         }
-        if (!req.query.userPassword) {
-            res.status(400).json({ error: 'Missing userPassword' })
-            return
-        }
-        if (!req.query.licenseId) {
-            res.status(400).json({ error: 'Missing licenseId' })
+        if (!req.query.signedMessage) {
+            res.status(400).json({ error: 'Missing signedMessage' })
             return
         }
         verify(req, res)
