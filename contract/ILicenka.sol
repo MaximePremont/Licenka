d14e4dd17d5f9e676be61fcc6de78533be95a898
Slooth <victor.guyot@epitech.eu>
2022-11-27 01:26:32 +0100

    feat(contract): added new contract

--- contract/ILicenka.sol
+++ contract/ILicenka.sol
@@ -1,21 +1,10 @@
 pragma solidity >=0.8.0 <0.9.0;
 // SPDX-License-Identifier: MIT License
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-
 interface ILicenka {
-    function createLicence(address owner, string memory name, uint price, uint duration) external;
+    function createLicense(address owner, string memory name, uint price, uint duration) external;
 
     function subscribe(uint licenseId) external;
 
-    function subscribeWeb2(address owner, uint hash, uint licenseId) external;
-
     function verifySubscription(address owner, uint licenseId) external view returns(bool);
-
-    function verifySubscriptionWeb2(address owner, uint hash, uint licenseId) external view returns(bool);
-
-    function getLicenses(address owner) external view returns(uint[] memory);
-
-    function getSubscriptions(address owner) external view returns(uint[] memory);
 }
