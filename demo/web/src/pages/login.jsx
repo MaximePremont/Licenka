f404865a0c601ff69e1e57ea45e284a864ce0946
Nolann <62215577+Nolann71@users.noreply.github.com>
2022-11-27 13:10:53 +0100

    fix(oauth): fix demo url

--- demo/web/src/pages/login.jsx
+++ demo/web/src/pages/login.jsx
@@ -1,14 +1,14 @@
 import logo from "../assets/game-controller.png";
 import powered from "../assets/powered_licenka.png";
-import {useSearchParams} from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-  
+
 
 function Login() {
     const [queryParameters] = useSearchParams()
-    let navigate = useNavigate(); 
-    const routeChange = () =>{ 
-        let path = `http://localhost:3000/oauth?redirect=http://localhost:3001/auth&licence_id=7`; 
+    let navigate = useNavigate();
+    const routeChange = () => {
+        let path = `http://localhost:3000/oauth?redirect_uri=http://localhost:3001/auth&license_id=7`;
         window.location.replace(path);
     }
 
@@ -17,10 +17,10 @@ function Login() {
             <header className="App-header">
                 <h1 className="title">CloudGames</h1>
                 <img src={logo} className="App-logo" alt="logo" />
-                <button onClick={routeChange} id="login" className="button"/>
+                <button onClick={routeChange} id="login" className="button" />
                 <p id="buying-license"><a href="http://localhost:3000/approve?id=7&redirect=http://localhost:3001">DON'T HAVE A LICENSE?</a></p>
                 <p id="warning">{queryParameters.get("warning")}</p>
-                <img src={powered} className="licenka-power" alt="logo"/>
+                <img src={powered} className="licenka-power" alt="logo" />
             </header>
         </div>
     );
