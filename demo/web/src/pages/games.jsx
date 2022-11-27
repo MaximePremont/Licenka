4ed00e0c17ed13e2735653ccb3b5c933d02e5e00
Nolann <62215577+Nolann71@users.noreply.github.com>
2022-11-27 18:59:43 +0100

    fix(demo): /verify forn

--- demo/web/src/pages/games.jsx
+++ demo/web/src/pages/games.jsx
@@ -1,39 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Cookies from 'universal-cookie';
-import {Navigate} from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
-async function Games() {
+function Games() {
+    const [isLicenseVerifed, setIsLicenseVerifed] = useState(null);
     const cookies = new Cookies();
     console.log(cookies.get('nonce_signed'), cookies.get('address'))
     const nonce_signed = cookies.get('nonce_signed');
     const address = cookies.get('address');
 
-    const res = await fetch("http://localhost:8080/verify", {
-        method: "POST",
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Content-Type': 'application/json',
-        },
-        withCredentials: true,
-        credentials: 'same-origin',
-        body: JSON.stringify({
-            nonce_signed,
-            address
+    useEffect(() => {
+        fetch("http://localhost:8080/verify", {
+            method: "POST",
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json',
+            },
+            withCredentials: true,
+            credentials: 'same-origin',
+            body: JSON.stringify({
+                nonce_signed,
+                address
+            })
+        }).then(res => {
+            if (res.ok) {
+                setIsLicenseVerifed(true);
+            } else {
+                setIsLicenseVerifed(false);
+            }
+        }).catch(err => {
+            console.log(err)
         })
-    })
-    console.log(res)
-    if (res.ok) {
-        console.log("2")
-        return (
-            <h1 className="title">License verified</h1>
-        );
-    } else {
-        console.log("1")
-        return (
-            <Navigate to="/login?warning=License%20unverified" />
-        );
-    }
+    }, []);
 
+    return (
+        <div className="">
+            {isLicenseVerifed == null ?
+                <h1 className="title">Verification in progress</h1> :
+                isLicenseVerifed ?
+                    <h1 className="title">License verified</h1> :
+                    <Navigate to="/login?warning=License%20unverified" />
+            }
+        </div>
+    );
 }
 
 export default Games;
