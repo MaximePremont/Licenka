f27342c9f367b351c4d3c10d66eb22d984d22411
Nolann <62215577+Nolann71@users.noreply.github.com>
2022-11-27 14:07:21 +0100

    fix(oauth): fix redirect url

--- pages/oauth.js
+++ pages/oauth.js
@@ -61,8 +61,7 @@ const OauthPage = () => {
         const data = await res.json();
         const nonce = data.nonce;
         web3.eth.personal.sign(nonce, accounts[0]).then((signature) => {
-          console.log(signature)
-          window.location.replace(`${redirect_uri}?nonce_signed=${signature}?address=${accounts[0]}`);
+          window.location.replace(`${redirect_uri}?nonce_signed=${signature}&address=${accounts[0]}`);
         }).catch((err) => {
           console.log(err);
           window.location.replace(redirect_uri_error);
