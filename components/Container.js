344ed67a5a7419cf1ea68e96915acf3a12ebe4ef
Slooth <victor.guyot@epitech.eu>
2022-12-04 03:02:37 +0100

    fix(front): overflow hidden position

--- components/Container.js
+++ components/Container.js
@@ -2,11 +2,11 @@ const Container = ({fill, className, style, children}) => {
 
   return (
     <div
-    className={"inline-flex items-center justify-center shadow-lg shadow-gray-600 p-0.5 overflow-hidden rounded-lg bg-gradient-to-br from-primary to-secondary disabled:cursor-not-allowed"}
+    className={"h-min flex items-center justify-center shadow-lg shadow-gray-600 p-0.5  rounded-lg bg-gradient-to-br from-primary to-secondary disabled:cursor-not-allowed"}
     >
       <span
         style={style}
-        className={className + " transition-all ease-in duration-100 " + (fill ? "" : " bg-background rounded-md")}>
+        className={className + " transition-all ease-in duration-100 overflow-hidden" + (fill ? "" : " bg-background rounded-md")}>
           {children}
       </span>
     </div>
